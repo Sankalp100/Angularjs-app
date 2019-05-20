b0babd3d1662e03762387fda6ede5063820f6cbd
Sankalp100 <sankalpgour123@gmail.com>
2019-05-20 16:23:16 +0530

    filters

--- New folder/app/app.js
+++ New folder/app/app.js
@@ -1,11 +1,23 @@
 var myGameApp = angular.module('myGameApp',[]);
 
-myGameApp.config(function(){
-
-});
-
-myGameApp.run(function(){
-
-});
-
-myGameApp.controller
+myGameApp.controller("GameController",['$scope', function($scope){
+   
+    
+    $scope.games = [
+        {
+            name: "tony",
+            game: "pubg",
+            rate: 50
+        },
+        {
+            name: "grim",
+            game: "cs:go",
+            rate: 10
+        },
+        {
+            name: "nova",
+            game: "fifa",
+            rate: 100
+        }
+    ];
+}]);
