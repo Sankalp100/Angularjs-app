4a71d09c714ed4ddd5997d3dcc56d47f1755238c
Sankalp100 <sankalpgour123@gmail.com>
2019-05-31 14:33:33 +0530

    Reactive Forms

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -11,7 +11,11 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule} from '@angular/material/input';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
@@ -59,7 +63,10 @@ import { LoginComponent } from './login/login.component';
     MatCheckboxModule,
     FlexLayoutModule,
     FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule,
+    MatSelectModule,
+    MatSlideToggleModule
 
   ],
 
