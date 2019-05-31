6634695e49aff17a48330e78e9f60faa1669ed27
Sankalp100 <sankalpgour123@gmail.com>
2019-05-31 13:18:56 +0530

    template-driven Forms

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -8,6 +8,11 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from  '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule} from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -48,8 +53,14 @@ import { LoginComponent } from './login/login.component';
     MatCardModule,
     MatButtonModule,
     MatDialogModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCheckboxModule,
     FlexLayoutModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+
   ],
 
   entryComponents: [
